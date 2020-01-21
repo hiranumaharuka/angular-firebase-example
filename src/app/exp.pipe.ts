f385fdc7d17ef72e13dae5b62c0764e0f19f998c
hiranumaharuka <colorcord251@gmail.com>
2020-01-21 14:51:18 +0900

    feat(exp-pipe):経験値表示パイプ追加

---
+++ src/app/exp.pipe.ts
@@ -0,0 +1,36 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Pet } from './interfaces/pet';
+
+const expTable = [
+  // Lv2に行くまでに必要な経験値
+  20, // 1
+  // Lv3に行くまでに必要な経験値
+  40, // 1
+  100, // 1
+  250, // 1
+  500, // 1
+  1000, // 1
+  1500, // 1
+  4000, // 1
+  10000 // 1
+
+];
+
+@Pipe({
+  name: 'exp'
+})
+export class ExpPipe implements PipeTransform {
+
+  transform(pet: Pet, ...args: any[]): any {
+    // pet.expには生まれたときからの経験値の総量が入ってる
+    const totalExp = pet.exp;
+    const level = pet.level;
+    // 今持ってる経験値
+    const baseExp = expTable[level - 2] || 0;
+    // 次の経験値になるのに必要な値を計算する
+    const nextExp = expTable[level - 1] - baseExp;
+    const exp = totalExp - baseExp;
+    return exp + ' / ' + nextExp;
+  }
+
+}
